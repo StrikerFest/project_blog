f030a7269b7a51c87786997f36fd1856e3891bf8
The Anh <Trinhtheanh789@Gmail.com>
2024-07-08 23:17:55 +0700

    - Update database files

--- database/install.php
+++ database/install.php
@@ -21,6 +21,13 @@ $sql = "
         `title` VARCHAR(255) NOT NULL,
         `content` TEXT NOT NULL
     );
+    
+    CREATE TABLE IF NOT EXISTS `users` (
+        `id` INT AUTO_INCREMENT PRIMARY KEY,
+        `username` VARCHAR(255) NOT NULL UNIQUE,
+        `password` VARCHAR(60) NOT NULL,
+        `type` SMALLINT
+    );
 ";
 
 // Execute SQL script
--- database/seed.php
+++ database/seed.php
@@ -21,6 +21,16 @@ for ($i = 1; $i <= 10; $i++) {
     }
 }
 
+// Generate user
+$sql = "INSERT INTO users (username, password, type) VALUES
+    ('admin', '$2a$12$9msGes.EQ1t3kEvK/HnWi.tb8O2wDtVLYXcvRGE/IhV2DgEoV0A4a', 1),
+    ('user', '$2a$12$89RY.tomk.SecxkYTb4E6uQEAd7yWSTrLU4VFV1wP456XsQhCxVcO', 2)";
+if ($conn->query($sql) === TRUE) {
+    echo "Post inserted successfully.<br>";
+} else {
+    echo "Error inserting post: " . $conn->error . "<br>";
+}
+
 // Close MySQL connection
 $conn->close();
 
