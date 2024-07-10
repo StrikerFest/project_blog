f1e4170d7176f4bc839a8aefd80738ca9fa50c5b
The Anh <Trinhtheanh789@Gmail.com>
2024-07-10 22:00:22 +0700

    - Merge và chỉnh đường dẫn cate và tag

--- index.php
+++ index.php
@@ -23,16 +23,16 @@ $routes = [
     '/admin/post/delete' => Common::getControllerPath('admin/post/delete.php'),
 
     // Quản lý danh mục
-    '/admin/category'        => Common::getControllerPath('category/index.php'),
-    '/admin/category/create' => Common::getControllerPath('category/edit.php'),
-    '/admin/category/edit'   => Common::getControllerPath('category/edit.php'),
-    '/admin/category/delete' => Common::getControllerPath('category/delete.php'),
+    '/admin/category'        => Common::getControllerPath('admin/category/index.php'),
+    '/admin/category/create' => Common::getControllerPath('admin/category/edit.php'),
+    '/admin/category/edit'   => Common::getControllerPath('admin/category/edit.php'),
+    '/admin/category/delete' => Common::getControllerPath('admin/category/delete.php'),
 
     // Quản lý nhãn
-    '/admin/tag'        => Common::getControllerPath('tag/index.php'),
-    '/admin/tag/create' => Common::getControllerPath('tag/edit.php'),
-    '/admin/tag/edit'   => Common::getControllerPath('tag/edit.php'),
-    '/admin/tag/delete' => Common::getControllerPath('tag/delete.php'),
+    '/admin/tag'        => Common::getControllerPath('admin/tag/index.php'),
+    '/admin/tag/create' => Common::getControllerPath('admin/tag/edit.php'),
+    '/admin/tag/edit'   => Common::getControllerPath('admin/tag/edit.php'),
+    '/admin/tag/delete' => Common::getControllerPath('admin/tag/delete.php'),
     
     // Blog - Người dùng
     '/login' => Common::getControllerPath('user/auth/login.php'),
