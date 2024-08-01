82995bed1958b4163c7a9c14af98f8906473e291
The Anh <Trinhtheanh789@Gmail.com>
2024-08-01 16:38:32 +0700

    Minor - User - Update auth style

--- templates/user/auth/register.php
+++ templates/user/auth/register.php
@@ -12,52 +12,48 @@ if (isset($_SESSION['user_frontend'])) {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign Up</title>
-    <link rel="stylesheet" href="<?= Common::getAssetPath('css/login-user.css') ?>">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script>
-        $(document).ready(function () {
-            $('#signup-form').submit(function (e) {
-                const password = $('#password').val();
-                const confirmPassword = $('#confirm-password').val();
-                if (password !== confirmPassword) {
-                    e.preventDefault();
-                    $('#password-error').text('Passwords do not match.').show();
-                } else {
-                    $('#password-error').hide();
-                }
-            });
-        });
-    </script>
-</head>
+<?php
+Common::requireTemplate('user/layouts/headers.php', [
+    'title' => 'Sign Up'
+]);
+?>
+
+<link rel="stylesheet" href="<?= Common::getAssetPath('css/signup-user.css') ?>">
 <body>
-<div class="sign-up">
-    <div class="circle circle--red"></div>
-    <div class="circle circle--yellow"></div>
-    <div class="circle circle--green"></div>
-    <div class="circle circle--purple"></div>
-    <form id="signup-form" class="sign-up__form" method="POST">
-        <div class="sign-up__content">
-            <h2 class="sign-up__title">Sign Up</h2>
-            <input class="sign-up__inp" type="text" name="username" placeholder="Username" required="required"/>
-            <input class="sign-up__inp" type="email" name="email" placeholder="Email" required="required"/>
-            <input class="sign-up__inp" type="password" id="password" name="password" placeholder="Password" required="required"/>
-            <input class="sign-up__inp" type="password" id="confirm-password" name="confirm_password" placeholder="Confirm Password" required="required"/>
-            <span id="password-error" class="error" style="display:none;"></span>
-        </div>
-        <?php if (isset($_SESSION['error_signup'])) { ?>
-            <div class="error"><?= $_SESSION['error_signup'];
-                unset($_SESSION['error_signup']); ?></div>
-        <?php } ?>
-        <div class="sign-up__buttons">
-            <button class="btn btn--signup" type="submit">Sign Up</button>
-        </div>
-    </form>
+<?php Common::requireTemplate('user/layouts/menu.php', []); ?>
+<div class="user-signup-wrapper">
+    <div class="user-signup-container">
+        <form id="signup-form" class="user-signup-form" method="POST">
+            <div class="user-signup-content">
+                <h2 class="user-signup-title">Sign Up</h2>
+                <input class="user-signup-input" type="text" name="username" placeholder="Username" required="required"/>
+                <input class="user-signup-input" type="email" name="email" placeholder="Email" required="required"/>
+                <input class="user-signup-input" type="password" id="password" name="password" placeholder="Password" required="required"/>
+                <input class="user-signup-input" type="password" id="confirm-password" name="confirm_password" placeholder="Confirm Password" required="required"/>
+                <span id="password-error" class="user-signup-error" style="display:none;"></span>
+            </div>
+            <?php if (isset($_SESSION['error_signup'])) { ?>
+                <div class="user-signup-error"><?= $_SESSION['error_signup'];
+                    unset($_SESSION['error_signup']); ?></div>
+            <?php } ?>
+            <div class="user-signup-buttons">
+                <button class="user-signup-btn" type="submit">Sign Up</button>
+            </div>
+        </form>
+    </div>
 </div>
+<script>
+    $(document).ready(function () {
+        $('#signup-form').submit(function (e) {
+            const password = $('#password').val();
+            const confirmPassword = $('#confirm-password').val();
+            if (password !== confirmPassword) {
+                e.preventDefault();
+                $('#password-error').text('Passwords do not match.').show();
+            } else {
+                $('#password-error').hide();
+            }
+        });
+    });
+</script>
 </body>
-</html>
