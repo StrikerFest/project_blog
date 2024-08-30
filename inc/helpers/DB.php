fc12604e600449af86593f4ce6ba3752e3378e9f
The Anh <Trinhtheanh789@Gmail.com>
2024-08-30 14:06:03 +0700

    Minor - DB - Fix charset not set to UTF8

--- inc/helpers/DB.php
+++ inc/helpers/DB.php
@@ -12,6 +12,9 @@ class DB {
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
+        if (!$conn->set_charset("utf8")) {
+            die("Error loading character set utf8: " . $conn->error);
+        }
         return $conn;
     }
 
